85c82480d319bf0a2d92503230b83010a3528540
B33rTrash3r <konstantinkulehov@gmail.com>
2025-09-04 23:36:33 +0300

    added custom inputs styles

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -14,26 +14,29 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Please fill out the following fields to login:</p>
-
+    <div class="form-group">
+        <input type="email" id="email" class="input-l" placeholder="">
+        <label for="email" class="input-label">AAAAAAA</label>
+    </div>
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-                <div class="my-1 mx-0" style="color:#999;">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                    <br>
-                    Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
-                </div>
+            <div class="my-1 mx-0" style="color:#999;">
+                If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+                <br>
+                Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
+            </div>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
+            <div class="form-group">
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
 
             <?php ActiveForm::end(); ?>
         </div>
